251079e33b96c777ca6d5a0df4f87ca88166812f
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-09-13 23:27:32 +0200

    Add calculator states

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,34 +3,25 @@ import './Calculator.css';
 import Buttons from './button';
 import calculate from '../logic/calculate';
 
-// const Calculator = () => (
-//   <div className="calculator">
-//     <div className="outputs">0</div>
-//     <div className="keys">
-//       <Buttons />
-//     </div>
-//   </div>
-// );
-
 const Calculator = () => {
-  const [calculatorData, setCalculatorData] = useState({
+  const [calculatorState, setCalculatorState] = useState({
     total: null,
     next: null,
     operation: null,
   });
 
-  const handleClick = (buttonName) => {
-    const newData = calculate(calculatorData, buttonName);
-    setCalculatorData(newData);
+  const handleButtonClick = (buttonName) => {
+    const newCalculatorState = calculate(calculatorState, buttonName);
+    setCalculatorState(newCalculatorState);
   };
 
   return (
     <div className="calculator">
       <div className="outputs">
-        {calculatorData.next || calculatorData.total || '0'}
+        {calculatorState.next || calculatorState.total || '0'}
       </div>
       <div className="keys">
-        <Buttons onClick={handleClick} />
+        <Buttons clickHandler={handleButtonClick} />
       </div>
     </div>
   );
