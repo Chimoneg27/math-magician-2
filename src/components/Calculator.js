448f677ec695f7113d01cbcf4d3bbf3982b157e1
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-09-13 18:35:36 +0200

    Update calculator and button JSX

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,39 @@
+import React, { useState } from 'react';
 import './Calculator.css';
 import Buttons from './button';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="calculator">
-    <div className="outputs">0</div>
-    <div className="keys">
-      <Buttons />
+// const Calculator = () => (
+//   <div className="calculator">
+//     <div className="outputs">0</div>
+//     <div className="keys">
+//       <Buttons />
+//     </div>
+//   </div>
+// );
+
+const Calculator = () => {
+  const [calculatorData, setCalculatorData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (buttonName) => {
+    const newData = calculate(calculatorData, buttonName);
+    setCalculatorData(newData);
+  };
+
+  return (
+    <div className="calculator">
+      <div className="outputs">
+        {calculatorData.next || calculatorData.total || '0'}
+      </div>
+      <div className="keys">
+        <Buttons onClick={handleClick} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Calculator;
--- src/components/button.js
+++ src/components/button.js
@@ -1,35 +1,57 @@
-const Buttons = () => (
-  <div className="rows">
-    <div className="row1">
-      <button type="button">AC</button>
-      <button type="button">+/-</button>
-      <button type="button">%</button>
-      <button type="button" className="far-right">÷</button>
+// const Buttons = () => (
+//   <div className="rows">
+//     <div className="row1">
+//       <button type="button">AC</button>
+//       <button type="button">+/-</button>
+//       <button type="button">%</button>
+//       <button type="button" className="far-right">÷</button>
+//     </div>
+//     <div className="row1">
+//       <button type="button">7</button>
+//       <button type="button">8</button>
+//       <button type="button">9</button>
+//       <button type="button" className="far-right">x</button>
+//     </div>
+//     <div className="row1">
+//       <button type="button">4</button>
+//       <button type="button">5</button>
+//       <button type="button">6</button>
+//       <button type="button" className="far-right">-</button>
+//     </div>
+//     <div className="row1">
+//       <button type="button">1</button>
+//       <button type="button">2</button>
+//       <button type="button">3</button>
+//       <button type="button" className="far-right">+</button>
+//     </div>
+//     <div className="bottom">
+//       <button type="button" className="zero">0</button>
+//       <button type="button" className="point">.</button>
+//       <button type="button" className="far-right">+</button>
+//     </div>
+//   </div>
+// );
+import PropTypes from 'prop-types';
+
+const Buttons = ({ onClick }) => {
+  const handleButtonClick = (buttonName) => {
+    onClick(buttonName);
+  };
+
+  return (
+    <div className="rows">
+      <div className="row1">
+        <button type="button" onClick={() => handleButtonClick('1')}>1</button>
+        <button type="button" onClick={() => handleButtonClick('2')}>2</button>
+        <button type="button" onClick={() => handleButtonClick('3')}>3</button>
+        <button type="button" className="far-right">÷</button>
+      </div>
     </div>
-    <div className="row1">
-      <button type="button">7</button>
-      <button type="button">8</button>
-      <button type="button">9</button>
-      <button type="button" className="far-right">x</button>
-    </div>
-    <div className="row1">
-      <button type="button">4</button>
-      <button type="button">5</button>
-      <button type="button">6</button>
-      <button type="button" className="far-right">-</button>
-    </div>
-    <div className="row1">
-      <button type="button">1</button>
-      <button type="button">2</button>
-      <button type="button">3</button>
-      <button type="button" className="far-right">+</button>
-    </div>
-    <div className="bottom">
-      <button type="button" className="zero">0</button>
-      <button type="button" className="point">.</button>
-      <button type="button" className="far-right">+</button>
-    </div>
-  </div>
-);
+  );
+};
+
+Buttons.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default Buttons;
