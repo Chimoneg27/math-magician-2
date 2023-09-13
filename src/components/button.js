929a5ffabc8efa2991865367b1679eec900df9eb
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-09-13 23:28:12 +0200

    Add event handlers

--- src/components/button.js
+++ src/components/button.js
@@ -1,57 +1,41 @@
-// const Buttons = () => (
-//   <div className="rows">
-//     <div className="row1">
-//       <button type="button">AC</button>
-//       <button type="button">+/-</button>
-//       <button type="button">%</button>
-//       <button type="button" className="far-right">÷</button>
-//     </div>
-//     <div className="row1">
-//       <button type="button">7</button>
-//       <button type="button">8</button>
-//       <button type="button">9</button>
-//       <button type="button" className="far-right">x</button>
-//     </div>
-//     <div className="row1">
-//       <button type="button">4</button>
-//       <button type="button">5</button>
-//       <button type="button">6</button>
-//       <button type="button" className="far-right">-</button>
-//     </div>
-//     <div className="row1">
-//       <button type="button">1</button>
-//       <button type="button">2</button>
-//       <button type="button">3</button>
-//       <button type="button" className="far-right">+</button>
-//     </div>
-//     <div className="bottom">
-//       <button type="button" className="zero">0</button>
-//       <button type="button" className="point">.</button>
-//       <button type="button" className="far-right">+</button>
-//     </div>
-//   </div>
-// );
 import PropTypes from 'prop-types';
 
-const Buttons = ({ onClick }) => {
-  const handleButtonClick = (buttonName) => {
-    onClick(buttonName);
-  };
-
-  return (
-    <div className="rows">
-      <div className="row1">
-        <button type="button" onClick={() => handleButtonClick('1')}>1</button>
-        <button type="button" onClick={() => handleButtonClick('2')}>2</button>
-        <button type="button" onClick={() => handleButtonClick('3')}>3</button>
-        <button type="button" className="far-right">÷</button>
-      </div>
+const Buttons = ({ clickHandler }) => (
+  <div className="rows">
+    <div className="row1">
+      <button type="button" onClick={() => clickHandler('AC')}>AC</button>
+      <button type="button" onClick={() => clickHandler('+/-')}>+/-</button>
+      <button type="button" onClick={() => clickHandler('%')}>%</button>
+      <button type="button" className="far-right" onClick={() => clickHandler('÷')}>÷</button>
     </div>
-  );
-};
+    <div className="row1">
+      <button type="button" onClick={() => clickHandler('7')}>7</button>
+      <button type="button" onClick={() => clickHandler('8')}>8</button>
+      <button type="button" onClick={() => clickHandler('9')}>9</button>
+      <button type="button" className="far-right" onClick={() => clickHandler('x')}>x</button>
+    </div>
+    <div className="row1">
+      <button type="button" onClick={() => clickHandler('4')}>4</button>
+      <button type="button" onClick={() => clickHandler('5')}>5</button>
+      <button type="button" onClick={() => clickHandler('6')}>6</button>
+      <button type="button" className="far-right" onClick={() => clickHandler('-')}>-</button>
+    </div>
+    <div className="row1">
+      <button type="button" onClick={() => clickHandler('1')}>1</button>
+      <button type="button" onClick={() => clickHandler('2')}>2</button>
+      <button type="button" onClick={() => clickHandler('3')}>3</button>
+      <button type="button" className="far-right" onClick={() => clickHandler('+')}>+</button>
+    </div>
+    <div className="bottom">
+      <button type="button" className="zero" onClick={() => clickHandler('0')}>0</button>
+      <button type="button" className="point" onClick={() => clickHandler('.')}>.</button>
+      <button type="button" className="far-right" onClick={() => clickHandler('=')}>=</button>
+    </div>
+  </div>
+);
 
 Buttons.propTypes = {
-  onClick: PropTypes.func.isRequired,
+  clickHandler: PropTypes.func.isRequired,
 };
 
 export default Buttons;
