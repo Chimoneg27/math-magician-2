1cb199b3fb9028e8398463b86e0245a8e1e1dc9b
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-09-14 22:59:27 +0200

    Add quote generator component

---
+++ src/components/quotes.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import GetQuote from '../API/API';
+import './quote.css';
+
+const Quotes = () => {
+  const [quotes, setQuote] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    GetQuote().then((res) => {
+      setQuote(res[0]);
+      setIsLoading(false);
+    }).catch(() => {
+      setIsError(true);
+    });
+  }, []);
+
+  const message = isError ? 'Request Failed!\nBroken Link' : 'Getting quote...';
+  return (
+    <div className="quotes-box">
+      {(isError || isLoading) ? <p className="message">{message}</p> : (
+        <blockquote>
+          <span className="quote">{quotes.quote}</span>
+          <cite>{quotes.author}</cite>
+        </blockquote>
+      )}
+    </div>
+  );
+};
+
+export default Quotes;
