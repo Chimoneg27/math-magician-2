0296e59c8802ae35de1b513eaacba354e0cbadd9
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-09-15 17:24:51 +0200

    remove comments

--- src/API/API.js
+++ src/API/API.js
@@ -15,21 +15,3 @@ const GetQuote = async () => {
 };
 
 export default GetQuote;
-/*
-const QUOTE_API = 'https://api.api-ninjas.com/v1/quotes?category=success';
-const API_KEY = 'RpfsaV7nmDsDUjhBFuoirw==1nbR3TwipCYKqTtC';
-const fetchQuote = async () => {
-  const response = await fetch(QUOTE_API, {
-    method: 'GET',
-    headers: {
-      'X-Api-key': API_KEY,
-      'Content-Type': 'application/json',
-    },
-  });
-  const data = await response.json();
-  if (data.error) throw data.error;
-  return data;
-};
-
-export default fetchQuote;
-*/
