38be4aa12030d75e3e621ce96ac3c357c5143ddf
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-09-20 01:44:08 +0200

    Add modified components with routes

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import '../styles/Home.css';
+
+const Home = () => (
+  <div className="Home-page">
+    <h1>Welcome To Our Page</h1>
+
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium quisquam unde vel
+      explicabo doloribus. Aliquid a accusantium omnis hic voluptate repellat et expedita
+      voluptas cupiditate id optio, exercitationem, maiores temporibus!
+      In maxime eius dolorum et error unde similique, suscipit deserunt nobis
+      id eum iste quia molestias aliquam reiciendis illo, obcaecati qui? Ab
+      adipisci molestias temporibus repellat tempore, beatae velit rem.
+      Nam totam distinctio officia rem? Deserunt deleniti suscipit dolorem
+      harum est natus dolore velit, reiciendis, qui nam id ab! Minima, facilis?
+    </p>
+
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium quisquam unde vel
+      explicabo doloribus. Aliquid a accusantium omnis hic voluptate repellat et expedita
+      voluptas cupiditate id optio, exercitationem, maiores temporibus!
+      In maxime eius dolorum et error unde similique, suscipit deserunt nobis
+      id eum iste quia molestias aliquam reiciendis illo, obcaecati qui? Ab
+      adipisci molestias temporibus repellat tempore, beatae velit rem.
+      Nam totam distinctio officia rem? Deserunt deleniti suscipit dolorem
+      harum est natus dolore velit, reiciendis, qui nam id ab! Minima, facilis?
+    </p>
+  </div>
+);
+
+export default Home;
